a5db4b0b20f2aa59d1368b205033fa3c29e0adef
evolveWD <vincenzo.bevivino@gmail.com>
2021-04-21 00:31:02 +0200

    aggiornamento 2104

--- src/mock/data.js
+++ src/mock/data.js
@@ -28,67 +28,65 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'ast.png',
+    title: 'AssistecTorino Store',
+    info: 'Wordpress e Woocommerce',
+    info2: 'Vendita online di articoli e servizi al privato e professionista. Tecnologie elettriche ed elettroniche, Home Automation, Safety e Comfort',
+    url: 'https://www2.assistectorino.it',
+    //repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'llc.png',
+    title: 'Luca Lobina Concept Association',
+    info: 'Wordpress e Woocommerce, multilingua',
+    info2: 'La LLC Association, oltre a voler offrire messaggi intelligenti, creativi e positivi nella società, sostiene opere di beneficenza e progetti di formazione, permettendo  anche a nuove menti intelligenti e creative, di proporre la propria idea o il proprio progetto al fine di realizzarlo, integrandolo nelle attività dell’associazione stessa.',
+    url: 'https://www2.assistectorino.it',
+    //repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
+  
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'Hai un progetto di cui vuoi parlare?',
+  btn: 'Scrivimi',
+  email: 'vincenzo.bevivino@gmail.com',
 };
 
 // FOOTER DATA
 export const footerData = {
   networks: [
+    
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
+      name: 'facebook',
+      url: 'https://www.facebook.com/enzo.bevivino',
     },
     {
       id: nanoid(),
-      name: 'codepen',
-      url: '',
+      name: 'instagram',
+      url: 'https://www.instagram.com/evolvewd_/',
+    },
+    {
+      id: nanoid(),
+      name: 'twitter',
+      url: 'https://twitter.com/evolvewd_',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/vincenzo-bevivino/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/evolvewd',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
